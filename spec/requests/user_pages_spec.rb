e1a75e1fc38ead5e93bf918ad6d0aa88eee0bf78
John Harvard <jharvard@appliance.localdomain>
2013-11-20 20:46:09 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,6 +11,8 @@ describe "User pages" do
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
+  
+  describe "with valid information" do
 
    describe "signup" do
 
@@ -34,7 +36,16 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
-      end
-    end
+     end
+     describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end     
+   end
   end
+ end
 end
